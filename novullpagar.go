5f09a7a8a0b673582ec8c667b06f74611686a169
Daniel Matilla <daniel@esdis.es>
2018-12-17 21:59:28 +0100

    Solver linter warnings

--- novullpagar.go
+++ novullpagar.go
@@ -5,49 +5,54 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strconv"
-	"strings"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-const tgbot_token = "PUT_BOT_TOKEN_HERE"
-const tgbot_log_id = -170311435
-const tgbot_chan_id = -1001347869136
+const tgbotToken = "PUT_BOT_TOKEN_HERE"
+const tgbotLogID = -170311435
+const tgbotChanID = -1001347869136
 const datasource = "https://www.meneame.net/sneakme_rss?q=%23novullpagar&w=posts&h=&o=date&u="
-const dynamodb_region = "eu-west-1"
-const dynamodb_table = "NoVullPagar"
+const dynamodbRegion = "eu-west-1"
+const dynamodbTable = "NoVullPagar"
 
-var hosts_allowed = [5]string{"humblebundle.com", "www.humblebundle.com", "www.gog.com", "gog.com", "store.steampowered.com"}
+var hostsAllowed = [5]string{"humblebundle.com", "www.humblebundle.com", "www.gog.com", "gog.com", "store.steampowered.com"}
 
+// Post structure for DynamoDB post table
 type Post struct {
 	ID     int `json:"id"`
-	PostId int `json:"PostId"`
+	PostID int `json:"PostId"`
 }
 
+// Enclosure structure for reading XML
 type Enclosure struct {
-	Url    string `xml:"url,attr"`
+	URL    string `xml:"url,attr"`
 	Length int64  `xml:"length,attr"`
 	Type   string `xml:"type,attr"`
 }
 
+// Item structure for reading XML
 type Item struct {
 	Title     string    `xml:"title"`
 	Link      string    `xml:"link"`
 	Desc      string    `xml:"description"`
-	Guid      string    `xml:"guid"`
+	GUID      string    `xml:"guid"`
 	Enclosure Enclosure `xml:"enclosure"`
 	PubDate   string    `xml:"pubDate"`
 }
 
+// Channel structure for reading XML
 type Channel struct {
 	Title string `xml:"title"`
 	Link  string `xml:"link"`
@@ -55,29 +60,30 @@ type Channel struct {
 	Items []Item `xml:"item"`
 }
 
+// Rss structure for reading XML
 type Rss struct {
 	Channel Channel `xml:"channel"`
 }
 
 func tglog(message string) {
-	bot, _ := tgbotapi.NewBotAPI(tgbot_token)
-	msg := tgbotapi.NewMessage(tgbot_log_id, fmt.Sprint(message))
+	bot, _ := tgbotapi.NewBotAPI(tgbotToken)
+	msg := tgbotapi.NewMessage(tgbotLogID, fmt.Sprint(message))
 	bot.Send(msg)
 }
 
 func tgsend(message string) {
-	bot, _ := tgbotapi.NewBotAPI(tgbot_token)
-	msg := tgbotapi.NewMessage(tgbot_chan_id, fmt.Sprint(message))
+	bot, _ := tgbotapi.NewBotAPI(tgbotToken)
+	msg := tgbotapi.NewMessage(tgbotChanID, fmt.Sprint(message))
 	bot.Send(msg)
 }
 
-func getLastPost() (int) {
+func getLastPost() int {
 	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String(dynamodb_region)},
+		Region: aws.String(dynamodbRegion)},
 	)
 	svc := dynamodb.New(sess)
 	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(dynamodb_table),
+		TableName: aws.String(dynamodbTable),
 		Key: map[string]*dynamodb.AttributeValue{
 			"id": {
 				N: aws.String(strconv.Itoa(1)),
@@ -89,12 +95,12 @@ func getLastPost() (int) {
 	}
 	post := Post{}
 	err = dynamodbattribute.UnmarshalMap(result.Item, &post)
-	return post.PostId
+	return post.PostID
 }
 
 func updateLastPost(postid int) {
 	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String(dynamodb_region)},
+		Region: aws.String(dynamodbRegion)},
 	)
 	svc := dynamodb.New(sess)
 	post := Post{1, postid}
@@ -104,7 +110,7 @@ func updateLastPost(postid int) {
 	}
 	input := &dynamodb.PutItemInput{
 		Item:      av,
-		TableName: aws.String(dynamodb_table),
+		TableName: aws.String(dynamodbTable),
 	}
 	_, err = svc.PutItem(input)
 	if err != nil {
@@ -117,13 +123,13 @@ func processPost(desc string) {
 	split := re.Split(desc, -1)
 	for i := range split {
 		runes := []rune(split[i])
-		if (string(runes[0:4]) == "http") {
+		if string(runes[0:4]) == "http" {
 			u, err := url.Parse(split[i])
 			if err != nil {
 				tglog(err.Error())
 			}
-			for _, allowed := range hosts_allowed {
-				if (u.Host == allowed) {
+			for _, allowed := range hostsAllowed {
+				if u.Host == allowed {
 					tgsend(split[i])
 				}
 			}
@@ -133,29 +139,30 @@ func processPost(desc string) {
 
 func process() {
 	resp, err := http.Get(datasource)
+	if err != nil {
+		return
+	}
 	defer resp.Body.Close()
 	rss := Rss{}
 	decoder := xml.NewDecoder(resp.Body)
 	decoder.Strict = false
 	err = decoder.Decode(&rss)
-	if err != nil {
-		return
-	}
-	lastPostId := getLastPost()
-	newLastPostId := lastPostId
+
+	lastPostID := getLastPost()
+	newlastPostID := lastPostID
 	for _, item := range rss.Channel.Items {
 		params := strings.Split(item.Link, "/")
 		id, _ := strconv.Atoi(params[len(params)-1])
-		if (id > lastPostId) {
+		if id > lastPostID {
 			processPost(item.Desc)
 
-			if (id > newLastPostId) {
-				newLastPostId = id
+			if id > newlastPostID {
+				newlastPostID = id
 			}
 		}
 	}
-	lastPostId = newLastPostId
-	updateLastPost(lastPostId)
+	lastPostID = newlastPostID
+	updateLastPost(lastPostID)
 }
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
